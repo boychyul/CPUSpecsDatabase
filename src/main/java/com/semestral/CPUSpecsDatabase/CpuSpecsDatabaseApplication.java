31ead7063b5adcf078a8aa69638d5617e262c50c
boychyul <boychyul@fit.cvut.cz>
2022-05-28 17:12:25 +0200

    Start project

---
+++ src/main/java/com/semestral/CPUSpecsDatabase/CpuSpecsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.semestral.CPUSpecsDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuSpecsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuSpecsDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semestral/CPUSpecsDatabase/CpuSpecsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semestral.CPUSpecsDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuSpecsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
